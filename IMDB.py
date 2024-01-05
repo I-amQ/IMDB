2419cf36069597970bdcce8561d0e425bd7dc674
Kuan <truongvanquang110101@gmail.com>
2024-01-05 12:56:50 +0100

    Selenium usage

--- IMDB.py
+++ IMDB.py
@@ -1,9 +1,27 @@
 import base64
 import io
+
+from selenium.common import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+
 import requests
 from PIL import Image
 from bs4 import BeautifulSoup
 import psycopg2
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+
+WINDOW_SIZE = "1920,1080"
+chrome_options = Options()
+#chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+
+chrome_options.binary_location = "C:/Program Files/Google/Chrome/Application/chrome.exe"
+web_driver = webdriver.Chrome(options=chrome_options)
+
+
 
 from urllib.request import Request, urlopen
 
@@ -68,11 +86,22 @@ def show_image(id):
 
     return image_str
 
+def get_movies(id):
+
+    conn = psycopg2.connect(dbname='IMDB_INFO', user='postgres', password='admin')
+    cur = conn.cursor()
+    query = 'SELECT movies FROM "Actors" WHERE "ID" = ' + str(id)
+    cur.execute(query)
+    rows = cur.fetchone()
+    cur.close()
+    conn.close()
+
+    return rows[0]
 
 def scrape_full_description(description_url):
 
     site = description_url
-    print(description_url)
+    #print(description_url)
     hdr = {'User-Agent': 'Mozilla/5.0'}
     req = Request(site, headers=hdr)
     page = urlopen(req)
@@ -86,9 +115,71 @@ def scrape_full_description(description_url):
         return "None"
 
 
+def scrape_all_time_movies(description_url):
+
+    global web_driver
+    site = description_url
+    #print(description_url)
+    #hdr = {'User-Agent': 'Mozilla/5.0'}
+    #req = Request(site, headers=hdr)
+    #page = urlopen(req)
+    #soup = BeautifulSoup(page, 'html.parser')
+
+    web_driver.delete_all_cookies()
+    web_driver.get(site)
+    #wait = WebDriverWait(web_driver, 10)
+
+    #try:
+    WebDriverWait(web_driver, 100).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="__next"]/div/div/div[2]/div/button[1]'))).click()
+    #decline_cookie_btn = web_driver.find_element(By.XPATH, '')
+    #decline_cookie_btn.click()
+    #except NoSuchElementException:
+    #    pass
+    #web_driver.execute_script("arguments[0].scrollIntoView();", EC.visibility_of_element_located((By.XPATH, '//*[@id="accordion-item-actor-previous-projects"]/div/div/span/button')))
+    web_driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
+
+    try:
+        (WebDriverWait(web_driver, 10).until(EC.visibility_of_element_located((By.XPATH,
+                                                                              '/html/body/div[2]/main/div/section[1]/div/section/div/div[1]/div[4]/section[2]/div[5]/div[2]/div/div[4]/div/div/span/button')))
+                                                        .click())
+    except Exception:
+        try:
+            button = web_driver.find_element(By.XPATH,
+                                             '//*[@id="accordion-item-actor-previous-projects"]/div/div/span/button')
+            web_driver.execute_script("arguments[0].click();", button)
+        except Exception:
+            try:
+                button = web_driver.find_element(By.XPATH,
+                                                 '/html/body/div[2]/main/div/section[1]/div/section/div/div[1]/div[4]/section[2]/div[5]/div[2]/div/div[4]/div/div/span/button')
+                web_driver.execute_script("arguments[0].click();", button)
+            except Exception:
+                button = web_driver.find_element(By.CLASS_NAME, 'ipc-see-more__button')
+                web_driver.execute_script("arguments[0].click();", button)
+    #button = web_driver.find_element(By.XPATH, '//*[@id="accordion-item-actor-previous-projects"]/div/div/span/button')
+
+    #web_driver.execute_script("arguments[0].scrollIntoView();", button)
+    #web_driver.execute_script("arguments[0].click();", button)
+
+    #button.click()
+
+    soup = BeautifulSoup(web_driver.page_source, 'html.parser')
+    actor_movies_section = soup.find('div', {'class': 'sc-6703147-3 jZGlkr'})
+    movies_tags = actor_movies_section.find_all('a',{'class': 'ipc-metadata-list-summary-item__t'})
+
+    movies = [movie.text.strip() for movie in movies_tags]
+
+    return movies
+
+
+def scrape_average_rating(description_url):
+        # TODO: Implement this method
+
+
+
+        pass
 
 def scrape_data():
-
+    global web_driver
     # Connect to your postgres DB
     conn = psycopg2.connect(dbname='IMDB_INFO', user='postgres', password='admin')
     cur = conn.cursor()
@@ -97,44 +188,47 @@ def scrape_data():
 
     # Get the webpage
     url = 'https://www.imdb.com/list/ls053501318/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    #response = requests.get(url)
+    web_driver.get(url)
+    soup = BeautifulSoup(web_driver.page_source, 'html.parser')
 
     actors = soup.find_all('div', class_='lister-item mode-detail')
 
     if actors is not None:
-
-        cur.execute('''TRUNCATE TABLE "Actors" RESTART IDENTITY;''')
+        cur.execute('''
+            TRUNCATE TABLE "Actors" RESTART IDENTITY;
+        ''')
 
         for actor in actors:
 
             # Get the actor name
             name = actor.find('h3').find('a').text.strip()
             # Get the actor description
-            description_url = actor.find('h3').find('a', href = True)
-
-            description_url = "https://www.imdb.com" + description_url['href'] + "/bio"
+            bio_link = actor.find('h3').find('a', href = True)
+            description_url = "https://www.imdb.com" + bio_link['href'].split("?")[0] + "/bio"
             print(description_url)
-
             description = scrape_full_description(description_url)
+            movies_link = "https://www.imdb.com" + bio_link['href'].split("?")[0] + "/"
+            print(movies_link)
+            movies = scrape_all_time_movies(movies_link)
 
-
-            #description = actor.find_all('p')[1].text.strip()
             # Get the actor image
             image_div = actor.find('div', class_='lister-item-image')
-
 
             if image_div is not None:
                 image_tag = image_div.find('img')
                 if image_tag is not None:
                     image_url = image_tag['src']
+
                     # Download the image
                     image_response = requests.get(image_url)
                     image = Image.open(io.BytesIO(image_response.content))
+
                     # Convert the image to binary format
                     buffered = io.BytesIO()
                     image.save(buffered, format="JPEG")
                     img_str = base64.b64encode(buffered.getvalue())
+
                 else:
                     img_str = None
             else:
@@ -142,8 +236,8 @@ def scrape_data():
                 img_str = None
 
             cur.execute('''
-                    INSERT INTO "Actors" (name, description, image) VALUES (%s, %s, %s)
-                ''', (name, description, img_str))
+                    INSERT INTO "Actors" (name, description, image, movies) VALUES (%s, %s, %s, %s)
+                ''', (name, description, img_str,(movies,)))
 
             conn.commit()
 
@@ -156,20 +250,30 @@ class IMDB:
     def all_time_movies(self, name):
         # TODO: Implement this method
 
+
+
         pass
 
     def awards(self, name):
         # TODO: Implement this method
+
+
+
         pass
 
     def movie_genre(self, name):
         # TODO: Implement this method
+
+
+
         pass
 
-    def average_rating(self, name):
-        # TODO: Implement this method
-        pass
+
 
     def top_movies(self, name):
         # TODO: Implement this method
+
+
+
+
         pass
--- main.py
+++ main.py
@@ -29,6 +29,24 @@ position_right = int(screen_width / 2 - window_width / 2)
 root.geometry(f"{window_width}x{window_height}+{position_right}+{position_top}")
 
 
+def on_select_movies(id):
+
+    # Create new window to show actor movies
+    movie_info_window = Toplevel(root)
+    movie_info_window.title(IMDB.show_name(id) + "'s movies")
+    movie_info_window.configure(background='Gray')  # Set the background color of the info window
+
+    movies = IMDB.get_movies(id)
+
+    text = tk.Text(movie_info_window, wrap='word', font=("Helvetica", 12), bg='black',
+                   fg='white')  # Set the background and foreground color of the text widget
+    for i, movie in enumerate(movies):
+        text.insert('end', str(i) + ". " + movie + "\n\n\n")
+    text.configure(state='disabled')
+    text.pack(expand=True, fill='both')
+    text.tag_configure("center", justify='center')
+
+
 def on_select(id):
 
     description = IMDB.show_info(id)
@@ -53,12 +71,26 @@ def on_select(id):
     label.image = img  # Keep a reference to the image
     label.pack()
 
+    button_frame = tk.Frame(info_window)
+    button_frame.pack()
+
+    movies_button = tk.Button(button_frame, text="Movies", command=lambda: on_select_movies(id))
+    movies_button.pack(side=tk.LEFT)
+
+    movies_button = tk.Button(button_frame, text="Genres")
+    movies_button.pack(side=tk.LEFT)
+
+    movies_button = tk.Button(button_frame, text="Awards")
+    movies_button.pack(side=tk.LEFT)
+
     text = tk.Text(info_window, wrap='word', font=("Helvetica", 12), bg='black',
                    fg='white')  # Set the background and foreground color of the text widget
     text.insert('end', description)
     text.configure(state='disabled')
     text.pack(expand=True, fill='both')
     text.tag_configure("center", justify='center')
+
+
 
 
 frame = tk.Frame(root, bg='yellow')  # Set the background color of the frame
